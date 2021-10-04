83058bbdbd9974a763147ffd30f9898ca64f8f7a
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-04 14:15:26 -0700

    chore: update

--- src/components/HomeHero/HomeHero.tsx
+++ src/components/HomeHero/HomeHero.tsx
@@ -6,7 +6,7 @@ export const HomeHero = () => {
       <div className="pt-32 md:pt-40 pb-12 md:pb-20">
         <div className="pb-12 md:pb-16 text-center">
           <h1
-            className="mb-4 text-6xl md:text-8xl font-extrabold leading-relaxed text-blueGray-800"
+            className="mb-4 text-5xl md:text-7xl font-extrabold leading-relaxed text-blueGray-700"
             data-aos="zoom-y-out"
             data-aos-delay="300"
           >
@@ -17,15 +17,18 @@ export const HomeHero = () => {
               data-aos-delay="450"
             >
               metaverse
+            </span>{" "}
+            <span role="img" aria-label="sheep">
+              🥳
             </span>
           </h1>
           <div className="mx-auto max-w-3xl">
             <div
-              className="sm:flex sm:justify-center mx-auto mt-12 md:mt-16 max-w-xs sm:max-w-none"
+              className="sm:flex sm:justify-center mx-auto mt-12 max-w-xs sm:max-w-none"
               data-aos="zoom-y-out"
-              data-aos-delay="900"
+              data-aos-delay="600"
             >
-              <WalletConnect className="py-4 px-8 text-xl text-blueGray-100 bg-gradient-to-r from-pink-300 hover:from-pink-200 via-purple-300 hover:via-purple-200 to-indigo-400 hover:to-indigo-300 rounded-lg border-2 border-trueGray-400">
+              <WalletConnect className="py-4 px-8 text-xl text-blueGray-100 bg-gradient-to-r from-pink-300 hover:from-pink-200 via-purple-300 hover:via-purple-200 to-indigo-400 hover:to-indigo-300 rounded-lg border-2 border-trueGray-400 duration-200 hover:scale-110">
                 Connect wallet
               </WalletConnect>
             </div>
