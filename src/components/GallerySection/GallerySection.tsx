0242f9dff271ee767e8a11eb22a83f6b6f259a63
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-07 16:45:28 -0700

    chore: update

--- src/components/GallerySection/GallerySection.tsx
+++ src/components/GallerySection/GallerySection.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
 
 import { GalleryMirror } from "@/components/GalleryMirror";
+import { GalleryNFT } from "@/components/GalleryNFT";
 import { GalleryPoap } from "@/components/GalleryPoap";
 import { GallerySnapshot } from "@/components/GallerySnapshot";
 import { GalleryToken } from "@/components/GalleryToken";
@@ -10,6 +11,7 @@ export const GallerySection: FC = () => {
   return (
     <div className="flex flex-col bg-blueGray-100">
       <ProfileHero />
+      <GalleryNFT />
       <GalleryPoap />
       <GalleryMirror />
       <GallerySnapshot />
