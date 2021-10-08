bf79ec6a4ef0e599993d7a447fbdaad42bf80a5e
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-08 16:06:32 -0700

    perf: ini swr values

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -8,15 +8,34 @@ import type {
 import { SWRConfig } from "swr";
 
 import { GalleryScreen } from "@/components/GalleryScreen";
-import { ENS_SWR, TOKEN_SWR } from "@/const/swr";
+import {
+  ENS_SWR,
+  OPENSEA_SWR,
+  POAP_SWR,
+  SNAPSHOT_SWR,
+  TOKEN_SWR,
+} from "@/const/swr";
 import { resolveEnsName } from "@/libs/ens";
+import { fetchOpenseaAssets } from "@/libs/opensea";
+import { fetchPoaps } from "@/libs/poap";
+import { fetchSnapshots } from "@/libs/snapshot";
 import { fetchToken } from "@/libs/token";
+import type { OpenseaAsset } from "@/types/opensea";
+import type { Poap } from "@/types/poap";
+import type { Snapshot } from "@/types/snapshot";
 import type { Token } from "@/types/token";
 
 export interface Props {
   address: string;
   ensName?: string;
   token: Token;
+  snapshots: {
+    votes: Snapshot[];
+  };
+  poaps: {
+    accounts: { tokens: Poap[] };
+  };
+  assets: { assets: OpenseaAsset[] };
 }
 
 // eslint-disable-next-line @typescript-eslint/require-await
@@ -54,12 +73,18 @@ export const getStaticProps: GetStaticProps<Props> = async ({
   }
 
   const token = await fetchToken(address);
+  const snapshots = await fetchSnapshots(address);
+  const poaps = await fetchPoaps(address);
+  const assets = await fetchOpenseaAssets(address);
 
   return {
     props: {
       address: address,
       ensName: slug,
       token: token,
+      snapshots: snapshots,
+      poaps: poaps,
+      assets: assets,
     },
   };
 };
@@ -68,6 +93,9 @@ export const PageId = ({
   address,
   ensName,
   token,
+  snapshots,
+  poaps,
+  assets,
 }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element => {
   return (
     <SWRConfig
@@ -75,6 +103,9 @@ export const PageId = ({
         fallback: {
           [`${ENS_SWR + address}`]: ensName,
           [`${TOKEN_SWR + address}`]: token,
+          [`${SNAPSHOT_SWR + address}`]: snapshots,
+          [`${POAP_SWR + address}`]: poaps,
+          [`${OPENSEA_SWR + address}`]: assets,
         },
       }}
     >
