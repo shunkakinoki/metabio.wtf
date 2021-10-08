1764e82858acdcbf6fef5a315f0343e04bad62f3
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-07 20:25:55 -0700

    chore: update

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,19 +1,22 @@
+import { utils } from "ethers";
 import type {
   GetStaticProps,
   InferGetStaticPropsType,
   GetStaticPaths,
   GetStaticPropsContext,
 } from "next";
-
 import { SWRConfig } from "swr";
 
 import { GalleryScreen } from "@/components/GalleryScreen";
-
+import { TOKEN_SWR } from "@/const/swr";
 import { resolveEnsName } from "@/libs/ens";
+import { fetchToken } from "@/libs/token";
+import type { Token } from "@/types/token";
 
 export interface Props {
   address: string;
   ensName?: string;
+  token: Token;
 }
 
 // eslint-disable-next-line @typescript-eslint/require-await
@@ -37,22 +40,26 @@ export const getStaticProps: GetStaticProps<Props> = async ({
   }
 
   const slug = slugs[0];
+  let address: string;
 
   if (slug.endsWith(".eth")) {
-    const address = await resolveEnsName(slug);
-    if (address) {
-      return {
-        props: {
-          address: address,
-          ensName: slug,
-        },
-      };
-    }
+    address = await resolveEnsName(slug);
+  } else address = slug;
+
+  if (!utils.isAddress(address)) {
+    return {
+      notFound: true,
+      revalidate: 300,
+    };
   }
+
+  const token = await fetchToken(address);
 
   return {
     props: {
-      address: slug,
+      address: address,
+      ensName: slug,
+      token: token,
     },
   };
 };
@@ -60,9 +67,16 @@ export const getStaticProps: GetStaticProps<Props> = async ({
 export const PageId = ({
   address,
   ensName,
+  token,
 }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element => {
   return (
-    <SWRConfig value={{}}>
+    <SWRConfig
+      value={{
+        fallback: {
+          [`${TOKEN_SWR + address}`]: token,
+        },
+      }}
+    >
       <GalleryScreen />
     </SWRConfig>
   );
