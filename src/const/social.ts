110751cfbf8f035589b46695a349f76dc7053f8d
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-14 12:10:02 -0700

    chore: update

--- src/const/social.ts
+++ src/const/social.ts
@@ -1,2 +1,6 @@
 export const NOTION_MANIFESTO_LINK =
   "https://www.notion.so/metafam/Metabio-8746a5dff6ec4746adda5119dd1ec3ec";
+
+export const DISCORD_SOCIAL_LINK = "https://discord.gg/Sm7Fsasdqx";
+export const TWITTER_SOCIAL_LINK = "https://twitter.com/metabio_";
+export const GITHUB_SOCIAL_LINK = "https://github.com/sentrei/metabio.wtf";
