1fcf4f66bf08978917e32ed2ceb8b8234a81cf11
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-14 13:29:50 -0700

    chore: update

---
+++ prisma/migrations/20211014202349_ini/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `Wallet` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pins` (
+    `id` INTEGER NOT NULL,
+    `src` VARCHAR(191),
+    `value` VARCHAR(191),
+    `type` VARCHAR(191) NOT NULL,
+    `walletId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
