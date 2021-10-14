5ae5d0ffece269881de6404c1bbfc4158e63e05b
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-13 19:56:43 -0700

    perf: ini profile hero

--- src/components/ProfileHero/ProfileHero.tsx
+++ src/components/ProfileHero/ProfileHero.tsx
@@ -24,14 +24,12 @@ export const ProfileHero: FC = () => {
   }, [addressTruncated, asPath, ens, profileAddressTruncated, profileEns]);
 
   return (
-    <div className="px-4 sm:px-6 mx-auto max-w-3xl">
-      <div className="pt-20 pb-12 md:pb-20">
-        <h1 className="mb-4 text-5xl md:text-7xl font-extrabold leading-relaxed text-center text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-          {address}
-        </h1>
-        <div className="flex justify-center mt-12">
-          <ProfileHeroButton value={address} />
-        </div>
+    <div className="py-12 px-4 sm:px-6 mx-auto max-w-3xl">
+      <h1 className="mb-4 text-5xl md:text-7xl font-extrabold leading-relaxed text-center text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+        {address}
+      </h1>
+      <div className="flex justify-center mt-12">
+        <ProfileHeroButton value={address} />
       </div>
     </div>
   );
