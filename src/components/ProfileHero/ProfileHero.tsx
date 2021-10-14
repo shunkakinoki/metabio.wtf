0bdd6492be95a67d968b3349b6cadf6492719f87
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-14 11:57:20 -0700

    chore: update

--- src/components/ProfileHero/ProfileHero.tsx
+++ src/components/ProfileHero/ProfileHero.tsx
@@ -24,13 +24,15 @@ export const ProfileHero: FC = () => {
   }, [addressTruncated, asPath, ens, profileAddressTruncated, profileEns]);
 
   return (
-    <div className="px-4 sm:px-6 pt-12 pb-20 mx-auto max-w-xl md:max-w-3xl">
-      <h1 className="pb-6 text-6xl md:text-7xl font-extrabold leading-relaxed text-center text-transparent overflow-ellipsis bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-        {address}
-      </h1>
-      <div className="flex justify-center pb-6">
-        <ProfileHeroButton value={address} />
+    <section className="flex justify-center items-center">
+      <div className="px-4 sm:px-6 pt-12 pb-20 mx-auto max-w-xl md:max-w-3xl">
+        <h1 className="overflow-x-auto pb-6 text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-extrabold leading-relaxed text-center text-transparent overflow-ellipsis bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 line-clamp-1">
+          {address}
+        </h1>
+        <div className="flex justify-center pb-6">
+          <ProfileHeroButton value={address} />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
