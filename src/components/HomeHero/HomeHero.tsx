4fe738cca41c1dab6d715ce2178a90d73ec27f28
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-14 10:33:10 -0700

    chore: update

--- src/components/HomeHero/HomeHero.tsx
+++ src/components/HomeHero/HomeHero.tsx
@@ -14,7 +14,7 @@ const WalletConnect = dynamic(
 export const HomeHero = () => {
   return (
     <section className="flex justify-center items-center h-full">
-      <div className="px-4 mx-auto max-w-5xl">
+      <div className="px-4 pt-12 sm:pt-20 md:pt-24 mx-auto max-w-5xl">
         <div className="text-center">
           <h1 className="text-5xl sm:text-6xl md:text-7xl font-extrabold tracking-tight text-trueGray-700">
             <span className="block xl:inline">
