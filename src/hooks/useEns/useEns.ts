43f802e3753d3432eaf06854165bdc66d4fc1a8f
Shun Kakinoki <shunkakinoki@gmail.com>
2021-10-14 15:31:42 -0700

    chore: update

--- src/hooks/useEns/useEns.ts
+++ src/hooks/useEns/useEns.ts
@@ -6,6 +6,7 @@ import useSWR from "swr";
 import { addressAtom } from "@/atoms/address";
 import { ENS_SWR } from "@/const/swr";
 import { lookupEnsAddress } from "@/libs/ens";
+import { concatSwrPath, removeSwrPath } from "@/libs/utils";
 
 export const useEns = () => {
   const address = useRecoilValue(addressAtom);
@@ -14,11 +15,11 @@ export const useEns = () => {
     if (!address) {
       return null;
     }
-    return ENS_SWR + address;
+    return concatSwrPath(ENS_SWR, address);
   }, [address]);
 
   const { data, error, mutate } = useSWR<string>(key, address => {
-    return lookupEnsAddress(address.replace(ENS_SWR, ""));
+    return lookupEnsAddress(removeSwrPath(ENS_SWR, address));
   });
 
   return {
